bc67dd01d693383d9f5002f2197a5db585f0237f
Nebulaz7 <jp0712002@gmail.com>
2026-05-13 17:10:38 +0100

    Refactor code structure for improved readability and maintainability

--- vouch-app/app/page.tsx
+++ vouch-app/app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import CodeSection from "./components/CodeSection";
+import Features from "./components/Features";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Features />
       <CodeSection />
     </div>
   );
