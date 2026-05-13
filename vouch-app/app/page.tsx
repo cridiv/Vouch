acf9148d1506363c568235f9623923b3cb8a91fb
Nebulaz7 <jp0712002@gmail.com>
2026-05-13 04:25:01 +0100

    feat: add CodeSection component with animations to enhance the homepage

--- vouch-app/app/page.tsx
+++ vouch-app/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import CodeSection from "./components/CodeSection";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <CodeSection />
     </div>
   );
 }
